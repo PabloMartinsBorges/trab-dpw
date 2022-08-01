8924da30aa37139a598e81e07780f1d62fe465a9
PabloMartinsBorges <emanuelmartins435borges@gmail.com>
2022-08-01 19:25:48 -0300

    jogo da memoria na ppágina jogo

--- js/js.js
+++ js/js.js
@@ -55,3 +55,109 @@ function escreve_mes(){
  document.write(nome[mes]);
  }
 
+
+ // Código do Jogo
+
+ var maximo  = 16;
+var largura = 120;
+var altura  = 200;
+var margem  = 20;
+var viradas = 0;
+var ctx;
+var ordem   = []; // ordem em que as cartas aparecem
+var imagens = [
+    "../imagem/guarana3.png", "../imagem/guarana3b.png",
+    "../imagem/guarana5.png", "../imagem/guarana5b.png",
+    "../imagem/guarana4.png", "../imagem/guarana4b.png",
+    "../imagem/guarana7.png", "../imagem/guarana7b.png",
+    "../imagem/guarana-evento.png", "../imagem/guarana-eventob.png",
+    "../imagem/guarana8.jpg", "../imagem/guarana8b.jpg",
+    "../imagem/logo1.png", "../imagem/logo1b.png",
+    "../imagem/guarana6.png", "../imagem/guarana6b.png"
+]
+
+function embaralhaCartas(){
+    // Vertor ordenado de valores
+    var i, verso = [];
+    for (i = 0; i < maximo; i++) {
+        ordem[i]  = i;
+    }
+    // Embaralha as cartas
+    var n, tmp;
+    for (i = ordem.length; i;) {
+        n = Math.random() * i-- | 0;
+        tmp = ordem[n];
+        ordem[n] = ordem[i];
+        ordem[i] = tmp;
+    }
+}
+
+function viraCarta(canvas, event) {
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    nx = Math.floor(x / (largura + margem));
+    x_inf = (nx + 1) * margem + nx * largura;
+    x_sup = x_inf + largura;
+
+    // Verifica se o clique está na segunda linha
+    if (y > altura + margem){
+        nx += 8;
+        y_inf = altura + margem;
+        y_sup = y_inf + altura;
+    }
+    else {
+        y_inf = 0;
+        y_sup = altura;
+    }
+
+    if ((x >= x_inf && x <= x_sup) && 
+         (y >= y_inf && y <= y_sup)){
+        var ctx = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        var img = new Image();
+        img.src = imagens[ordem[nx]];
+        img.onload = function (){
+            ctx.drawImage(img, x_inf, y_inf, largura, altura);
+        }
+    }
+}
+
+function criaImagem(i, x, y){
+    var ctx = document.getElementById('canvas');
+    if (canvas.getContext){
+        var ctx = canvas.getContext('2d');
+        var img = new Image();
+        img.src = "../imagem/guarana2.png";
+        img.onload = function (){
+            ctx.drawImage(img, x, y, largura, altura);
+        }
+    }
+}
+
+function montaPainel(){
+    var x = margem;
+    var y = 0;
+    for (var i = 0; i < maximo; i++){
+        criaImagem(i, x, y);
+        x += largura + margem;
+        if (i > (maximo / 2) - 2 && y == 0){
+            x = margem;
+            y += altura + margem;
+        }
+    }
+}
+
+function init(){
+    // O metodo HTMLCanvasElement.getContext() retorna um contexto de desenho no canvas,
+    // ou null se o contexto identificado não é suportado
+    ctx = document.getElementById('canvas').getContext('2d');
+    ctx.font = "bold 20px sans-serif";
+    ctx.fillStyle = "blue";
+    ElementoCanvas = document.getElementById('canvas');
+    ElementoCanvas.addEventListener('mousedown', function(e){
+            viraCarta(ElementoCanvas, e);
+        });      
+    embaralhaCartas();
+    montaPainel();   
+}
